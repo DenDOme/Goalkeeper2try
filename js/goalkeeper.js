88865d2856832d64a6593db7d20af44adfb8140f
Den Dome <newlifeofkirim@gmail.com>
2023-12-10 05:34:02 +0700

    forth commit | game done | need animations

--- js/goalkeeper.js
+++ js/goalkeeper.js
@@ -1,17 +1,18 @@
 export default class Goalkeeper{
     constructor(){
         this.initGame();
-        this.name;
     }
     gameOptions = {
+        nickName : '',
         gkSpeed : 1000,
         forgeSpeed: 300,
         gkCurr : 400,
-        timer : 5 * 60,
+        timer : 1 * 60,
         timerMode : 'limited',
         xDir : 1,
-        lineCur:14,
         lDir : 1,
+        plScore : 0,
+        plStreak : 0,
     }
     startGame(){
         if(this.gameOptions.timerMode !== 'unlimited'){
@@ -21,7 +22,14 @@ export default class Goalkeeper{
         this.initGC();
     }
     stopGame(){
-
+        let getGameDisp = document.getElementById('game-screen');
+        let getEndDisp = document.getElementById('end-screen');
+        let getNameDisp = document.getElementById('playerName');
+        let getPointDisp = document.getElementById('endScore');
+        getGameDisp.style.display = 'none';
+        getEndDisp.style.display = 'flex';
+        getNameDisp.innerText = this.gameOptions.nickName;
+        getPointDisp.innerHTML = this.gameOptions.plScore;
     }
     timer(){
         let gameDisp = document.getElementById('body')
@@ -50,7 +58,7 @@ export default class Goalkeeper{
             seconds = seconds < 10 ? '0' + seconds : seconds;
             timerDisp.innerHTML = `${minutes}: ${seconds}`;
             this.gameOptions.timer--;
-            if(this.gameOptions.timer === -1){
+            if(this.gameOptions.timer == -1){
                 timerDisp.innerHTML = ''
                 clearInterval(realTimer);
                 this.stopGame();
@@ -72,7 +80,7 @@ export default class Goalkeeper{
 
         startGameBtn.addEventListener('click', () => {
             this.checkName(userInputText.value);
-            this.name = userInputText.value;
+            this.gameOptions.nickName = userInputText.value;
             this.timer();
             this.removeStartDisp();
             this.addGameDisp();
@@ -80,12 +88,6 @@ export default class Goalkeeper{
         })
     }
     gkMovement(){
-        let gameDisp = document.getElementById('gate');
-        let gk = document.createElement('div');
-        gk.classList.add('goalkeeper')
-        gk.id = 'goalkeeper';
-        gk.style.transform = "translateX(-50%)"
-        gameDisp.appendChild(gk);
         this.gameOptions.gkMove = setInterval(() => {
             let gk = document.getElementById('goalkeeper')
             this.gameOptions.gkCurr += this.gameOptions.xDir;
@@ -121,7 +123,7 @@ export default class Goalkeeper{
         getGameDisp.style.display = 'block';
     }
     addName(){
-        let personName = this.name;
+        let personName = this.gameOptions.nickName;
         let getUsernameDiv = document.getElementById('player');
         getUsernameDiv.innerHTML = personName;
     }
@@ -137,10 +139,10 @@ export default class Goalkeeper{
     }
     moveGC(event){
         document.getElementById('cursor').style.display = 'block';
-        document.getElementById('cursor').style.transform = 'translateY('+(event.clientY-640)+'px)';
-        document.getElementById('cursor').style.transform += 'translateX('+(event.clientX-30)+'px)';  
+        document.getElementById('cursor').style.transform = 'translateY('+(event.clientY-615)+'px)';
+        document.getElementById('cursor').style.transform += 'translateX('+(event.clientX-30)+'px)';
     }
-    forgeBar(pChance){
+    forgeBar(){
         let createForgeBar = document.getElementById('forge');
         let createForgeLine = document.createElement('div');
         let allLines = document.querySelectorAll('.line');
@@ -149,7 +151,8 @@ export default class Goalkeeper{
         }
         createForgeLine.classList.add('line');
         createForgeBar.appendChild(createForgeLine);
-        let lineCurr = 14; 
+        let lineCurr = 15; 
+        this.gameOptions.pChance = 1;
         this.gameOptions.fBar = setInterval(() => {
             lineCurr += this.gameOptions.lDir;
             createForgeLine.style.left = lineCurr + 'px';
@@ -159,14 +162,30 @@ export default class Goalkeeper{
             if(lineCurr <= 14){
                 this.changeDirL();
             }
+            if(lineCurr >= 14 && lineCurr <= 93){
+                this.gameOptions.pChance = 1
+            }
+            else if(lineCurr >= 94 && lineCurr <= 187){
+                this.gameOptions.pChance = 2
+            }
+            else if(lineCurr >= 188 && lineCurr <= 281){
+                this.gameOptions.pChance = 3
+            }
+            else if(lineCurr >= 282 && lineCurr <= 375){
+                this.gameOptions.pChance = 4
+            }
+            else if(lineCurr >= 376 && lineCurr <= 476){
+                this.gameOptions.pChance = 5
+            }
         }, this.gameOptions.forgeSpeed/70);
     }
     initGC(){
+        let createForceBar = document.getElementById('forge');
+        createForceBar.style.display = 'none';
         let flag = true;
         let pointChance = 0;
         let getGameDisp = document.getElementById('game-screen');
-        getGameDisp.addEventListener('mousemove',this.moveGC,false);
-        getGameDisp.addEventListener('click', ()=>{
+        const clickListener = () => {
             if(flag == true){
                 getGameDisp.removeEventListener('mousemove',this.moveGC,false);
                 let createForceBar = document.getElementById('forge');
@@ -176,10 +195,14 @@ export default class Goalkeeper{
             }
             else if(flag == false){
                 clearInterval(this.gameOptions.fBar);
+                clearInterval(this.gameOptions.gkMove)
                 this.checkGoal();
                 flag = true;
+                getGameDisp.removeEventListener('click' , clickListener);
             }
-        })
+        }
+        getGameDisp.addEventListener('mousemove',this.moveGC,false);
+        getGameDisp.addEventListener('click', clickListener)
     }
     changeDirL(){
         if(this.gameOptions.lDir == 1){
@@ -192,6 +215,53 @@ export default class Goalkeeper{
         }
     }
     checkGoal(){
-        
+        let getScoreDisp = document.getElementById('score');
+        let dispRect = document.getElementById('game-screen').getBoundingClientRect()
+        let gateRect = document.getElementById('gate').getBoundingClientRect()
+        let cursorRect = document.getElementById('cursor').getBoundingClientRect()
+        let gkRect = document.getElementById('goalkeeper').getBoundingClientRect()
+        let getActionDisp = document.getElementById('action');
+        let CursorX = cursorRect.left+30;
+        let CursorY = cursorRect.top+30;
+        if(((CursorX >= dispRect.left && CursorX <= gateRect.left)||(CursorX >= gateRect.right && CursorX <= dispRect.right))||((CursorY >= dispRect.top && CursorY <= gateRect.top)||(CursorY >= gateRect.bottom && CursorY <= dispRect.bottom))){
+            getActionDisp.innerHTML = 'out';
+            this.gameOptions.plStreak = 0;
+        }
+        else if((CursorX >= (gkRect.left - 50)&& CursorX <= (gkRect.right+50))&&(CursorY >= (gkRect.top-50)&&(CursorY <= (gkRect.bottom)))){
+            getActionDisp.innerHTML = 'gk catch'
+            this.gameOptions.plStreak = 0;
+        }
+        else{
+            let min = 1;
+            let max = 100;
+            let randomInt = this.getRandomIntInclusive(min,max);
+            let answ = false
+            if(this.gameOptions.plStreak >= 3){
+                this.gameOptions.pChance += 1;
+            }
+            if(randomInt <= (this.gameOptions.pChance * 10)){
+                answ = true
+            }
+            if(answ == true){
+                getActionDisp.innerHTML = 'goal';
+                this.gameOptions.plScore++;
+                this.gameOptions.plStreak++;
+                this.gameOptions.gkSpeed -= 10;
+                getScoreDisp.innerHTML = this.gameOptions.plScore;
+            }
+            else{
+                getActionDisp.innerHTML = 'catch';
+                this.gameOptions.plStreak = 0;
+            }
+        }
+        this.gameOptions.tempTimer = setTimeout(() => {
+            this.initGC();
+            this.gkMovement();
+        }, 1000);
     }
+    getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); 
+      }
 }
